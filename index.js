642ebe1a0b6055da329b7e2927276f6fcdc9b7f8
Rasik Bihari <Rasik.Bihari@1e.com>
2019-01-11 17:41:51 +0530

    refactoring: added method isEqual
    isEqual method has been added. Also, refactored few variable names.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function Game(event) {
   if (event.target.nodeName == "TD" && event.target.innerHTML == '' ) {
     event.target.innerHTML = nextSign;
     let dataIndex = event.target.getAttribute('data-index');
-    let splittedIndices = dataIndex.split(' ');
-    let rowVal = Number(splittedIndices[0]);
-    let colVal = Number(splittedIndices[1]);
+    let splitIndices = dataIndex.split(' ');
+    let rowVal = Number(splitIndices[0]);
+    let colVal = Number(splitIndices[1]);
 
     board[rowVal][colVal] = nextSign;
 
@@ -27,7 +27,7 @@ function Game(event) {
 function checkWinner(){
     //check for row winners
     for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
-        if (board[rowIndex][0] != '' && board[rowIndex][0] == board[rowIndex][1] && board[rowIndex][1] == board[rowIndex][2]) {
+        if (board[rowIndex][0] != '' && isEqual(board[rowIndex][0] , board[rowIndex][1] , board[rowIndex][2])) {
             alert('Player ' + board[rowIndex][0]  + ' is winner');
             disableGameBoard();
             return;
@@ -36,7 +36,7 @@ function checkWinner(){
 
     //check for column winners
     for (let colIndex = 0; colIndex < 3; colIndex++) {
-        if (board[0][colIndex] != '' && board[0][colIndex] == board[1][colIndex] && board[1][colIndex] == board[2][colIndex]) {
+        if (board[0][colIndex] != '' && isEqual(board[0][colIndex] , board[1][colIndex] , board[2][colIndex])) {
             alert('Player ' + board[0][colIndex]  + ' is winner');
             disableGameBoard();
             return;
@@ -44,19 +44,26 @@ function checkWinner(){
     }
 
     //check for slanting line winner
-    if (board[0][0] != '' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    if (board[0][0] != '' && isEqual(board[0][0] , board[1][1] , board[2][2])) {
         alert('Player ' + board[0][0]  + ' is winner');
         disableGameBoard();
         return;
     }
 
     //check for slanting line winner
-    if (board[0][2] != '' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+    if (board[0][2] != '' && isEqual(board[0][2] , board[1][1] , board[2][0])) {
         alert('Player ' + board[0][2]  + ' is winner');
         disableGameBoard();
         return;
     }
 
+}
+
+function isEqual(val1, val2, val3){
+    if(val1 == val2 && val2 == val3){
+        return true;
+    }
+    return false;
 }
 
 function disableGameBoard(){
